872f9e2839db0e76fd6088dcf16208263926a45f
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-01-07 13:37:50 +0100

    feat: Added Flowbite CSS

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  darkMode: 'class',
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+} satisfies Config
