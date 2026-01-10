52c5c28d8ba12cac1523b2d1856303a9a5ab4d77
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-01-10 01:59:24 +0100

    feat: Admin account database seeder

---
+++ database/seeders/AdminUserAndPermissionsSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminUuid = Hash::make('admin-uuid');
+
+        $user = User::create([
+            'uuid' => $adminUuid,
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'onboarding_status' => 1,
+            'onboarding_step' => 3,
+        ]);
+
+        DB::table('account_links')->insert([
+            'player_uuid' => 'e296a2b2-3062-49e8-bba0-77e625038c22',
+            'user_uuid' => $adminUuid,
+            'is_linked' => true,
+            'linked_at' => now(),
+        ]);
+
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Permissions
+        Permission::create(['name' => 'manage_permissions']);
+
+        $user->givePermissionTo('manage_permissions');
+    }
+}
