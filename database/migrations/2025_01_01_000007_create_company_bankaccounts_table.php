9e342acfd22a09ee236328ddac81ad515d92c6d4
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-01-10 16:10:45 +0100

    feat: Added currency columns to both back account tables.

--- database/migrations/2025_01_01_000007_create_company_bankaccounts_table.php
+++ database/migrations/2025_01_01_000007_create_company_bankaccounts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
             $table->decimal('balance', 8, 2);
             $table->boolean('is_main')->default(false);
+            $table->string('currency', 3);
             $table->timestamps();
         });
     }
