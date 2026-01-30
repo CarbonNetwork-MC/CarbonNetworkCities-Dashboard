7498a53f41900d27318470b56332d639a5f50ba9
milanhaaijer <milanhaaijer2004@gmail.com>
2026-01-30 19:02:37 +0100

    fix: added parameter to redirect

--- app/Livewire/Admin/PinConsoles/Edit.php
+++ app/Livewire/Admin/PinConsoles/Edit.php
@@ -89,12 +89,12 @@ class Edit extends Component
 
         if ($status !== 202) {
             $this->rollbackPinConsole($originalData);
-            return redirect()->route('admin.pin-consoles.edit')->error(__('admin.toast.pin_consoles.pin_console_update_failed'));
+            return redirect()->route('admin.pin-consoles.edit', ['id' => $this->pinConsole->id])->error(__('admin.toast.pin_consoles.pin_console_update_failed'));
         }
 
         if (!$success) {
             $this->rollbackPinConsole($originalData);
-            return redirect()->route('admin.pin-consoles.edit')->error(__('admin.toast.pin_consoles.pin_console_update_failed'));
+            return redirect()->route('admin.pin-consoles.edit', ['id' => $this->pinConsole->id])->error(__('admin.toast.pin_consoles.pin_console_update_failed'));
         }
 
         return redirect()->route('admin.pin-consoles.render')->success(__('admin.toast.pin_consoles.updated'));
