5b4a69625be337395d8e0fb175017396a7b7123d
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-02-13 22:54:27 +0100

    fix: renamed import because there were 2 components with the same name

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ use App\Livewire\Company\UpdateStock;
 
 use App\Livewire\Profile\Overview as ProfileOverview;
 
-use App\Livewire\Wholesale\ChooseCompany;
+use App\Livewire\Wholesale\ChooseCompany as WholesaleChooseCompany;
 use App\Livewire\Wholesale\CollectOrder;
 use App\Livewire\Wholesale\CompleteOrder;
 use App\Livewire\Wholesale\CreateOrder;
@@ -143,8 +143,8 @@ Route::middleware(['auth', 'onboarding'])->group(function() {
   
     // ? Wholesale
     Route::middleware('permission:wholesale_order')->group(function() {
-        Route::get('/wholesale/choose-company', ChooseCompany::class)->name('wholesale.choose-company');
         Route::get('/wholesale/create-order/{companyId}', CreateOrder::class)->name('wholesale.create-order');
+        Route::get('/wholesale/choose-company', WholesaleChooseCompany::class)->name('wholesale.choose-company');
     });
 
     Route::middleware('permission:manage_wholesale_orders')->group(function() {
