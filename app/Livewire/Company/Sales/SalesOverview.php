e5e712755e03dad5bd6d2779797fa8e4302da5db
Xandor Sportel <xandorzakelijk@gmail.com>
2026-02-17 12:08:59 +0100

    feat: inputs in sales page

--- app/Livewire/Company/Sales/SalesOverview.php
+++ app/Livewire/Company/Sales/SalesOverview.php
@@ -15,14 +15,43 @@ class SalesOverview extends Component
 
     public $products;
     public $sales = [];
+    public $total = 0;
 
     public function mount($companyId) {
-        $this->company = Company::where('id', $companyId)->with(['sales', 'sales.items'])->first();
+        $this->company = Company::where('id', $companyId)->with(['country', 'sales', 'sales.items'])->first();
 
         $this->currentYear = now()->year;
         $this->currentWeek = now()->weekOfYear;
 
         $this->products = CompanyItem::where('company_id', $this->company->id)->with(['stock', 'item'])->get();
+        foreach ($this->products as $index => $product) {
+            $this->sales[$index] = ['item' => $product->item->name, 'amount' => 0, 'max_amount' => 999, 'price' => 0];
+        }
+    }
+
+    public function increment($index, $amount = 1) {
+        $this->sales[$index]['amount'] += $amount;
+        if ($this->sales[$index]['amount'] < 0) {
+            $this->sales[$index]['amount'] = 0;
+        }
+        $this->calculatePrice($index);
+    }
+
+    public function decrement($index, $amount = 1) {
+        $this->sales[$index]['amount'] -= $amount;
+        if ($this->sales[$index]['amount'] < 0) {
+            $this->sales[$index]['amount'] = 0;
+        }
+        $this->calculatePrice($index);
+    }
+
+    public function calculatePrice($index) {
+        $this->sales[$index]['price'] = $this->sales[$index]['amount'] * $this->products[$index]->price;
+        $this->calculateTotal();
+    }
+
+    public function calculateTotal() {
+        $this->total = array_sum(array_column($this->sales, 'price'));
     }
 
     public function render()
--- resources/views/livewire/company/sales/sales-overview.blade.php
+++ resources/views/livewire/company/sales/sales-overview.blade.php
@@ -33,15 +33,80 @@
                 </div>
             </x-containers.main>
             <x-containers.main class="mt-4">
-                <div class="grid grid-cols-3 gap-x-4">
-                    @forelse ($products as $index => $product)
-                        <div class="col-span-1 text-md font-medium text-gray-700 dark:text-white">
-                            {{ $product->item->name }}
+                <div class="grid grid-cols-3 gap-4">
+                    @forelse ($sales as $index => $product)
+                        <div class="col-span-1 flex items-center text-md font-medium text-gray-700 dark:text-white">
+                            {{ $product['item'] }}
                         </div>
-                        <div class="col-span-1">
-                            <x-forms.number-input wire:model="sales.{{ $index }}.amount" />
+                        <div class="col-span-1 flex items-center gap-2">
+                            {{-- Decrement 1 --}}
+                            <button 
+                                type="button" 
+                                wire:click="decrement({{ $index }})" 
+                                class="text-gray-400 bg-gray-300 dark:bg-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-16 h-8 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer focus:outline-none focus:ring-0 focus:ring-transparent {{ $product['amount'] == 0 || empty($product['amount']) ? 'invisible' : '' }}"
+                                tabindex="-1"
+                            >
+                                <i class="fi fi-rr-minus-small"></i> <span class="text-md ms-0.5">1</span>
+                            </button>
+
+                            {{-- Decrement 32 --}}
+                            <button 
+                                type="button" 
+                                wire:click="decrement({{ $index }}, 32)" 
+                                class="text-gray-400 bg-gray-300 dark:bg-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-20 h-8 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer focus:outline-none focus:ring-0 focus:ring-transparent {{ $product['amount'] == 0 || empty($product['amount']) ? 'invisible' : '' }}"
+                                tabindex="-1"
+                            >
+                                <i class="fi fi-rr-minus-small"></i> <span class="text-md ms-0.5">32</span>
+                            </button>
+
+                            {{-- Decrement 64 --}}
+                            <button 
+                                type="button" 
+                                wire:click="decrement({{ $index }}, 64)" 
+                                class="text-gray-400 bg-gray-300 dark:bg-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-20 h-8 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer focus:outline-none focus:ring-0 focus:ring-transparent {{ $product['amount'] == 0 || empty($product['amount']) ? 'invisible' : '' }}"
+                                tabindex="-1"
+                            >
+                                <i class="fi fi-rr-minus-small"></i> <span class="text-md ms-0.5">64</span>
+                            </button>
+
+                            {{-- Amount input --}}
+                            <x-forms.number-input wire:model.live="sales.{{ $index }}.amount" wire:blur="calculatePrice('{{ $index }}')" class="w-10" />
+
+                            {{-- Increment 1 --}}
+                            <button 
+                                type="button" 
+                                wire:click="increment({{ $index }})"
+                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-16 h-8 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer focus:outline-none focus:ring-0 focus:ring-transparent {{ $product['amount'] >= $product['max_amount'] ? 'invisible' : '' }}"
+                                tabindex="-1"
+                            >
+                                <i class="fi fi-rr-plus-small"></i> <span class="text-md ms-0.5">1</span>
+                            </button>
+
+                            {{-- Increment 32 --}}
+                            <button 
+                                type="button" 
+                                wire:click="increment({{ $index }}, 32)"
+                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-16 h-8 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer focus:outline-none focus:ring-0 focus:ring-transparent {{ $product['amount'] >= $product['max_amount'] ? 'invisible' : '' }}"
+                                tabindex="-1"
+                            >
+                                <i class="fi fi-rr-plus-small"></i> <span class="text-md ms-0.5">32</span>
+                            </button>
+
+                            {{-- Increment 64 --}}
+                            <button 
+                                type="button" 
+                                wire:click="increment({{ $index }}, 64)"
+                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-16 h-8 flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer focus:outline-none focus:ring-0 focus:ring-transparent {{ $product['amount'] >= $product['max_amount'] ? 'invisible' : '' }}"
+                                tabindex="-1"
+                            >
+                                <i class="fi fi-rr-plus-small"></i> <span class="text-md ms-0.5">64</span>
+                            </button>
                         </div>
-                        <div class="col-span-1"></div>
+                        <div class="col-span-1 flex justify-end items-center">
+                            <div class="font-medium text-gray-700 dark:text-white">
+                                {{ $company->country->currency_symbol ?? '' }}{{ $sales[$index]['price'] ? number_format($sales[$index]['price'], 2) : number_format(0, 2) }}
+                            </div>
+                        </div>
                     @empty
 
                     @endforelse
