5ac3311c67107a7fb1772fe53e627cd37cc5d75a
Xandor Sportel <97609628+XandorSportel@users.noreply.github.com>
2026-03-11 21:07:35 +0100

    feat: new tip page

---
+++ app/Livewire/Company/Tips/NewTip.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Livewire\Company\Tips;
+
+use App\Models\Company;
+use App\Models\EmployeeSalaryUpdate;
+use App\Models\Player;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class NewTip extends Component
+{
+    public $company;
+
+    public $customer;
+    public $amount;
+
+    public function mount($companyId) {
+        $this->company = Company::where('id', $companyId)->first();
+    }
+
+    public function save() {
+        $this->validate([
+            'customer' => ['required', 'exists:players,username'],
+            'amount' => ['required', 'numeric', 'min:0.01'],
+        ]);
+
+        $customer = Player::where('username', $this->customer)->first();
+        if (!$customer) {
+            $this->addError('customer', __('company.validation.customer_not_found'));
+            return;
+        }
+
+        $tip = $this->company->tips()->create([
+            'employee_uuid' => auth()->user()->player->uuid,
+            'customer_uuid' => $customer->uuid,
+            'amount' => $this->amount,
+        ]);
+
+        $tip_scheme = $this->company->settings->tip_scheme;
+        if ($tip_scheme == 'per_employee') {
+            $existingSalary = $this->company->salaries()
+                ->where('player_uuid', Auth::user()->player->uuid)
+                ->where('year', now()->year)
+                ->where('week', now()->weekOfYear)
+                ->where('status', 'unpaid')
+                ->first();
+            if (!$existingSalary) {
+                $this->company->salaries()->create([
+                    'company_id' => $this->company->id,
+                    'player_uuid' => Auth::user()->player->uuid,
+                    'year' => now()->year,
+                    'week' => now()->weekOfYear,
+                    'amount' => $this->amount,
+                ]);
+            } else {
+                $existingSalary->amount += $this->amount;
+                $existingSalary->save();
+            }
+
+            EmployeeSalaryUpdate::create([
+                'player_uuid' => Auth::user()->player->uuid,
+                'tip_id' => $tip->id,
+                'amount' => $this->amount,
+            ]);
+        } else {
+            $splitAmount = floor($this->amount / $this->company->employees()->count() * 100) / 100;
+
+            // Employees
+            foreach ($this->company->employees as $employee) {
+                $existingSalary = $this->company->salaries()
+                    ->where('player_uuid', $employee->player_uuid)
+                    ->where('year', now()->year)
+                    ->where('week', now()->weekOfYear)
+                    ->where('status', 'unpaid')
+                    ->first();
+                if (!$existingSalary) {
+                    $this->company->salaries()->create([
+                        'company_id' => $this->company->id,
+                        'player_uuid' => $employee->player_uuid,
+                        'year' => now()->year,
+                        'week' => now()->weekOfYear,
+                        'amount' => $splitAmount,
+                    ]);
+                } else {
+                    $existingSalary->amount += $splitAmount;
+                    $existingSalary->save();
+                }
+
+                EmployeeSalaryUpdate::create([
+                    'player_uuid' => $employee->player_uuid,
+                    'tip_id' => $tip->id,
+                    'amount' => $splitAmount,
+                ]);
+            }
+
+            // Owner
+            $ownerExistingSalary = $this->company->salaries()
+                ->where('player_uuid', $this->company->owner_uuid)
+                ->where('year', now()->year)
+                ->where('week', now()->weekOfYear)
+                ->where('status', 'unpaid')
+                ->first();
+            if (!$ownerExistingSalary) {
+                $this->company->salaries()->create([
+                    'company_id' => $this->company->id,
+                    'player_uuid' => $this->company->owner_uuid,
+                    'year' => now()->year,
+                    'week' => now()->weekOfYear,
+                    'amount' => $splitAmount,
+                ]);
+            } else {
+                $ownerExistingSalary->amount += $splitAmount;
+                $ownerExistingSalary->save();
+            }
+
+            EmployeeSalaryUpdate::create([
+                'player_uuid' => $this->company->owner_uuid,
+                'tip_id' => $tip->id,
+                'amount' => $splitAmount,
+            ]);
+        }
+
+        return redirect()->route('company.tips.render', ['companyId' => $this->company->id, 'tipId' => $tip->id])->success('company.toasts.tip_created');
+    }
+
+    public function render() {
+        return view('livewire.company.tips.new-tip');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -92,6 +92,7 @@ use App\Livewire\Company\Sales\CreateSale;
 use App\Livewire\Company\Sales\SaleDetails;
 use App\Livewire\Company\Sales\SalesOverview;
 use App\Livewire\Company\Settings\Overview as CompanySettingsOverview;
+use App\Livewire\Company\Tips\NewTip;
 use App\Livewire\Company\Tips\Overview as TipsOverview;
 use App\Livewire\Company\WholesaleOrders\ChooseCompany;
 use App\Livewire\Company\WholesaleOrders\WholesaleOrders;
@@ -149,6 +150,7 @@ Route::middleware(['auth', 'onboarding'])->group(function() {
         Route::get('/company/{companyId}/sales/{saleId}', SaleDetails::class)->name('company.sales.details.render');
         Route::get('/company/{companyId}/stock', StockOverview::class)->name('company.stock.render');
         Route::get('/company/{companyId}/tips', TipsOverview::class)->name('company.tips.render');
+        Route::get('/company/{companyId}/tips/new', NewTip::class)->name('company.tips.new.render');
         Route::middleware('company_owner_or_manager')->group(function() {
             Route::get('/company/{companyId}/bank-account/{bankAccountId}', BankaccountOverview::class)->name('company.bank-account.render');
             Route::get('/company/{companyId}/choose-bank-account', ChooseBankaccount::class)->name('company.bank-accounts.render');
